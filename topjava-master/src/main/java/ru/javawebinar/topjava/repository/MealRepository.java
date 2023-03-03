c7542ddd71a4e3a723485a7a25050d76666f7e6d
nikita <nikitachertkov@gmail.com>
2023-03-03 11:17:37 +0500

    Initial commit

---
+++ topjava-master/src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.repository;
+
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface MealRepository {
+    // null if not found, when updated
+    // null if updated meal does not belong to userId
+    Meal save(Meal meal, int userId);
+
+    // false if not found
+    // false if meal does not belong to userId
+    boolean delete(int id, int userId);
+
+    // null if not found
+    // null if meal does not belong to userId
+    Meal get(int id, int userId);
+    // ORDERED dateTime desc
+    List<Meal> getAll(int userId);
+
+    // ORDERED dateTime desc
+    List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId);
+
+    default Meal getWithUser(int id, int userId) {
+        throw new UnsupportedOperationException();
+    }
+}
+
